aac9b97fbfeb4bd8c732d783e716815d4d883610
Dhiraj Wadile <114635434+Dhiraj-Wadile@users.noreply.github.com>
2024-02-26 12:30:01 +0530

    implement clerk webhooks

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,36 +1,32 @@
-import mongoose ,{Mongoose} from 'mongoose';
+import mongoose, { Mongoose } from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI ;
+const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
-    conn : Mongoose | null;
-    promise : Promise<Mongoose> | null;
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
 }
 
-let cached : MongooseConnection = (global as any).mongoose
+let cached: MongooseConnection = (global as any).mongoose
 
-if (!cached) {
-    cached = (global as any).mongoose =
-     { 
-        conn: null, promise: null 
-    }
+if(!cached) {
+  cached = (global as any).mongoose = { 
+    conn: null, promise: null 
+  }
 }
 
 export const connectToDatabase = async () => {
-    if (cached.conn) 
-        return cached.conn;
+  if(cached.conn) return cached.conn;
 
-    if(!MONGODB_URI) 
-        throw new Error(
-            'Missing MONGODB_URL'
-        );
-        
-        cached.promise=
-        cached.promise || 
-        mongoose.connect(MONGODB_URI, {   
-            dbName: 'imaginify', bufferCommands: false
-        })
+  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-        cached.conn = await cached.promise;
+  cached.promise = 
+    cached.promise || 
+    mongoose.connect(MONGODB_URL, { 
+      dbName: 'imaginify', bufferCommands: false 
+    })
 
-    }
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+}
